acee881c2930ea1d14c4c0c3d0b4945c04fe8893
Paul <pkilgarriff@gmail.com>
2022-02-26 15:56:57 +0000

    Update Van class  - add pick_up_broken_bikes method   - add deliver_broken_to_garage method

--- lib/van.rb
+++ lib/van.rb
@@ -1,11 +1,17 @@
 class Van
-	attr_reader :trunk
+  attr_reader :trunk
 
-	def initialize
-		@trunk = []
-	end
+  def initialize
+    @trunk = []
+  end
 
-	def pick_up_broken_bikes(station)
+  def pick_up_broken_bikes(station)
+    released = station.release_broken_to_van
+    @trunk = released
+  end
 
-	end
+  def deliver_broken_to_garage(garage)
+    garage.receive_bike(@trunk)
+    @trunk.clear
+  end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,11 +1,21 @@
 require 'van'
 
 describe Van do
-	let(:van) { Van.new }
-	let(:station) { DockingStation.new }
+  let(:van) { Van.new }
+  let(:station) { DockingStation.new }
+  let(:bike) { Bike.new }
+  let(:garage) { Garage.new }
 
-	it "picks up broken bikes from the station" do
-		van.pick_up_broken_bikes(station)
-		expect(van.trunk).to be_instance_of(Array)
-	end
+  it "picks up broken bikes from the station" do
+    station.dock_bike(bike, true)
+    van.pick_up_broken_bikes(station)
+    expect(van.trunk).to include(bike)
+  end
+
+  it 'delivers broken bikes to the garage' do
+    station.dock_bike(bike, true)
+    van.pick_up_broken_bikes(station)
+    van.deliver_broken_to_garage(garage)
+    expect(van.trunk).to be_empty
+  end
 end
